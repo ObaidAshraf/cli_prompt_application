048534f51f978bb6f5ec08dfbe8a7ae2e2ce0b6e
obaidashraf <ubaidashraf91@gmail.com>
2023-08-19 20:33:22 +0500

    Added inquirer package to get user input

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const inquirer = require('inquirer')
 const { OpenAI } = require('langchain/llms/openai');
 
 const model = new OpenAI({ 
@@ -6,15 +7,26 @@ const model = new OpenAI({
   temperature: 0,
   model: 'gpt-3.5-turbo'
 });
-
   
-const promptFunc = async () => {
+const promptFunc = async (input) => {
   try {
-    const res = await model.call("What color is the sky?");
+    const res = await model.call(input);
     console.log(res)
   } catch (error) {
     console.log(error)
   }
 }
 
-promptFunc()
+const init = () => {
+  inquirer.prompt([
+    {
+      type: 'init',
+      name: 'name',
+      message: 'Ask a question:'
+    }
+  ]).then((inquirerResponse) => {
+    promptFunc(inquirerResponse.name)
+  })
+}
+
+init()
