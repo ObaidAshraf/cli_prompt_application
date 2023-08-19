3ff92f36d9d0d25521b17a4ebbb061625cd9f552
obaidashraf <ubaidashraf91@gmail.com>
2023-08-19 20:24:05 +0500

    Added function to use model.call()

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ require('dotenv').config()
 const { OpenAI } = require('langchain/llms/openai');
 
 const model = new OpenAI({ 
-    openAIApiKey: process.env.OPENAI_API_KEY, 
-    temperature: 0,
-    model: 'gpt-3.5-turbo'
-  });
+  openAIApiKey: process.env.OPENAI_API_KEY, 
+  temperature: 0,
+  model: 'gpt-3.5-turbo'
+});
+
   
-  console.log({ model });
+const promptFunc = async () => {
+  try {
+    const res = await model.call("What color is the sky?");
+    console.log(res)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+promptFunc()
